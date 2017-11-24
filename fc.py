25cae252520e7e30df45fc8fcbc50b427d60d030
bigzhu <vermiliondun@gmail.com>
2017-11-24 23:35:20 +0800

    flask 的 session 简直太给力

--- fc.py
+++ fc.py
@@ -3,16 +3,45 @@
 import sys
 sys.path.append("../lib_py")
 
+import flask
 from flask import Flask
 from flask import request
-# from flask import make_response
 from flask import jsonify
 from flask_bz import ExtEncoder
 import message
+import oauth_bz
 
 
 app = Flask(__name__)
 app.json_encoder = ExtEncoder
+app.secret_key = 'just test'
+
+
+@app.route('/api_last')
+def api_last():
+
+    data = {
+        'registered_count': oauth_bz.getCount()
+    }
+
+    return jsonify(data)
+
+
+@app.route('/api_registered')
+def api_registered():
+
+    data = {
+        'registered_count': oauth_bz.getCount()
+    }
+
+    return jsonify(data)
+
+
+@app.route('/api_oauth_info')
+def api_oauth_info():
+
+    data = oauth_bz.getOauthInfo('5')
+    return jsonify(data)
 
 
 @app.route('/api_new')
@@ -26,14 +55,16 @@ def api_new():
     return jsonify(data)
 
 
-@app.route('/projects/')
-def projects():
-    return 'The project page'
+@app.route('/set')
+def set():
+    flask.session['user_id'] = '4'
+    flask.session['bigzhu'] = 'very big'
+    return 'kao'
 
 
-@app.route('/about')
-def about():
-    return 'The about page'
+@app.route('/get')
+def get():
+    return flask.session['user_id']
 
 
 if __name__ == '__main__':
