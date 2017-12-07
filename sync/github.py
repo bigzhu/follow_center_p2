05d243898fd27494007729f39488f0109b7910f4
bigzhu <vermiliondun@gmail.com>
2017-12-07 14:45:32 +0800

    github sync 可用

--- sync/github.py
+++ sync/github.py
@@ -5,22 +5,21 @@ create by bigzhu at 15/07/15 17:17:29 取github的动态
 '''
 import sys
 sys.path.append("../../lib_py")
+sys.path.append("../")
 
 import datetime
 import requests
-from db_bz import pg
 import sys
-from datetime import timedelta
-from public_bz import storage
 import god_oper
-import json
+import db_bz
 import time_bz
+from model import Message
 import time
-import public_bz
-import social_sync
+import exception_bz
+from sync.social_lib import loop
 
-import ConfigParser
-config = ConfigParser.ConfigParser()
+import configparser
+config = configparser.ConfigParser()
 with open('conf/github.ini', 'r') as cfg_file:
     config.readfp(cfg_file)
     client_id = config.get('secret', 'client_id')
@@ -30,29 +29,29 @@ params = {'client_id': client_id, 'client_secret': client_secret}
 
 def getGithubUser(github_name, god_name):
     try:
-        r = requests.get('https://api.github.com/users/%s' % github_name, params=params)
+        r = requests.get('https://api.github.com/users/%s' %
+                         github_name, params=params)
         github_user = r.json()
         message = github_user.get('message')
         if message == 'Not Found':
             god_oper.delNoName('github', god_name)
             return
-        saveUser(god_name, github_name, github_user)
         return github_user
     except requests.exceptions.ConnectionError:
-        print(public_bz.getExpInfoAll())
+        print(exception_bz.getExpInfoAll())
         return
     except ValueError:
-        print(public_bz.getExpInfoAll())
+        print(exception_bz.getExpInfoAll())
         return
 
 
-def main(god, wait):
+def sync(god, wait):
     '''
     create by bigzhu at 15/07/15 17:54:08 取github
     modify by bigzhu at 15/07/22 16:20:42 时间同样要加入8小时,否则不正确
     '''
-    god_name = god.name
     github_name = god.github['name']
+    god_name = god.name
     god_id = god.id
 
     etag = god.github.get('sync_key')
@@ -61,9 +60,10 @@ def main(god, wait):
         return
     headers = {'If-None-Match': etag}
     try:
-        r = requests.get('https://api.github.com/users/%s/events' % github_name, headers=headers, params=params)
+        r = requests.get('https://api.github.com/users/%s/events' %
+                         github_name, headers=headers, params=params)
     except requests.exceptions.ConnectionError:
-        print(public_bz.getExpInfoAll())
+        print(exception_bz.getExpInfoAll())
         return
     if r.status_code == 200:
         etag = r.headers['etag']
@@ -71,10 +71,9 @@ def main(god, wait):
         if limit == '0':
             return
         for message in r.json():
-            message = storage(message)
             saveMessage(god_name, github_name, god_id, message)
         # oper.noMessageTooLong('github', github_name)
-        saveUser(god_name, github_name, github_user, etag)  # 为了update etag
+        saveUser(god, github_user, etag)  # 为了update etag
     if r.status_code == 404:
         # public_db.sendDelApply('github', god_name, github_name, '404')
         god_oper.delNoName('github', god_name)
@@ -84,69 +83,61 @@ def saveMessage(god_name, github_name, god_id, message):
     '''
     create by bigzhu at 15/07/16 09:44:39 为了抽取数据方便,合并数据到 content 里
     '''
-    content = storage()
-    content.type = message.type
-    content.repo = message.repo
-    content.payload = message.payload
-    content = json.dumps(content)
+    content = dict(
+        type=message['type'],
+        repo=message['repo'],
+        payload=message['payload']
+    )
 
-    m = public_bz.storage()
-    m.god_id = god_id
-    m.god_name = god_name
-    m.name = github_name
-    # m.avatar = message.actor['avatar_url']
-
-    m.id_str = message['id']
-    m.m_type = 'github'
-    m.created_at = time_bz.unicodeToDateTIme(message.created_at)
-    m.created_at += timedelta(hours=8)
-    m.content = content
-    m.text = None
-    m.href = None
-    id = pg.insertIfNotExist('message', m, "id_str='%s' and m_type='github'" % m.id_str)
-    if id is not None:
-        print('%s new github %s' % (m.name, m.id_str))
+    m = dict(
+        god_id=god_id,
+        god_name=god_name,
+        name=github_name,
+        out_id=message['id'],
+        m_type='github',
+        out_created_at=time_bz.jsonToDatetime(message['created_at']),
+        # m.created_at += timedelta(hours=8)
+        content=content,
+        text=None,
+        href=None,
+    )
+    i, insert = db_bz.updateOrInsert(
+        Message, m, out_id=message['id'], m_type='github')
+    if insert:
+        print('%s new github %s' % (m['name'], m['out_id']))
     return id
 
 
-def saveUser(god_name, github_name, user, sync_key=None):
-    social_user = public_bz.storage()
-
-    try:
-        # social_user.name = user['login']
-        social_user.name = github_name
-    except Exception as e:
-        print(e)
-        print(user)
-    social_user.type = 'github'
+def saveUser(god, user, sync_key=None):
+    github = dict(
+        type='github',
+        name=god.github['name'],
+        avatar=user.get('avatar_url', ''),
+        description=user.get('bio'),
+        id=user['id'],
+        sync_key=sync_key
+    )
     if user.get('followers') is None:
-        social_user.count = -1
+        github['count'] = -1
     else:
-        social_user.count = user['followers']
-    social_user.avatar = user.get('avatar_url', '')
-    social_user.description = user.get('bio')
+        github['count'] = user['followers']
     if sync_key is not None:
-        social_user.sync_key = sync_key
+        github['sync_key'] = sync_key
 
-    pg.update('god', where={'name': god_name}, github=json.dumps(social_user))
-    return social_user
+    god.github = github
+    return True
 
 
-def loop(god_name=None, wait=None):
-    '''
-    '''
-    gods = social_sync.getSocialGods('github', god_name)
-    for god in gods:
-        main(god, wait)
+def main():
+    if len(sys.argv) == 2:
+        god_name = (sys.argv[1])
+        loop(sync, 'github', god_name)
+        exit(0)
+    while True:
+        loop(sync, 'github', wait=True)
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        time.sleep(1200)
 
 
 if __name__ == '__main__':
-    # print json.dumps(getGithubUser('kdzwinel', 'kdzwinel'))
-    if len(sys.argv) == 2:
-        user_name = (sys.argv[1])
-        loop(user_name)
-        exit(0)
-    while True:
-        loop(wait=True)
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        time.sleep(1200)
+    main()
