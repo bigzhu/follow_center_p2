d89ef04b3ce3462a9c02f7bf91681c7a88d846c1
bigzhu <vermiliondun@gmail.com>
2017-12-08 23:34:37 +0800

    just save last bigger

--- last_oper.py
+++ last_oper.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import db_bz
 from model import Last
+from db_bz import session
 
 
 def saveLast(user_id, last):
-    last_info = dict(updated_at=last)
-    db_bz.updateOrInsert(Last, last_info, user_id=user_id)
+    # last_info = dict(updated_at=last)
+    last_info = session.query(Last).filter(
+        Last.updated_at < last).filter(Last.user_id == user_id).one_or_none()
+    if last_info is not None:
+        last_info.updated_at = last
 
 
 if __name__ == '__main__':
